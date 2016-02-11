b06cfc60318680d30613dd706c94e360f32649f8
Vinco <vvincoo@gmail.com>
2016-02-11 20:07:04 +0100

    created parcels table

---
+++ database/migrations/2016_02_11_155139_create_parcels_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParcelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parcels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default(null);
+			$table->char('type', 1);
+			$table->decimal('gps_lon', 11, 7);
+			$table->decimal('gps_lat', 11, 7);
+			$table->integer('kadaster_id')->unsigned();
+			$table->integer('proceeding_id')->unsigned();	
+			$table->softDeletes();
+			
+			$table->foreign('kadaster_id')->references('id')->on('kadasters');
+			$table->foreign('proceeding_id')->references('id')->on('proceedings');
+			
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		Schema::table('parcels', function (Blueprint $table)
+        {
+            $table->dropForeign('parcels_kadaster_id_foreign');
+			$table->dropForeign('parcels_proceeding_id_foreign');
+        });
+		
+        Schema::drop('parcels');
+    }
+}
