0164faf51a701893e8457ebeb834cc2f70dcc05d
Vinco <vvincoo@gmail.com>
2016-03-11 00:37:01 +0100

    file prcesses seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,25 +1,28 @@
 <?php
-
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
 
-        Model::reguard();
+		// $this->call(UserTableSeeder::class);
+
+		Model::reguard();
 		$this->call('DistrictsTableSeeder');
-    	$this->call('CityDistrictsTableSeeder');
+		$this->call('CityDistrictsTableSeeder');
 		$this->call('ProceedingTypesTableSeeder');
 		$this->call('ProceedingPhasesTableSeeder');
+		$this->call('FileProcessesTableSeeder');
+
+		exec("mysql --host " . env('DB_HOST') . " -u " . env('DB_USERNAME') . " -p " . env('DB_PASSWORD') . " " . env('DB_DATABASE') . " < " .'"'.getcwd().'/database/sql/parcel_locations.sql'.'"');
 	}
 }
