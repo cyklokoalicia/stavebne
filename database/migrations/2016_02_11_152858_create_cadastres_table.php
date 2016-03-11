6f40177f107bd2e1749668f64ca1ea78bb538fed
Vinco <vvincoo@gmail.com>
2016-03-11 22:43:59 +0100

    removed bugs

--- database/migrations/2016_02_11_152858_create_cadastres_table.php
+++ database/migrations/2016_02_11_152858_create_cadastres_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCadastersTable extends Migration
+class CreateCadastresTable extends Migration
 {
     /**
      * Run the migrations.
